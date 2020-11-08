bba614bf62b6c2c0da3340dafdbb5c33cfc52583
gaitavr <tbiwka@yandex.ru>
2020-11-08 11:23:42 +0200

    Hologram shader

---
+++ Assets/Scripts/DisplacementEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Material _hologram;
+    [SerializeField]
+    private AnimationCurve _curve;
+
+    private void Start()
+    {
+        _curve.postWrapMode = WrapMode.Loop;
+    }
+    
+    private void Update()
+    {
+        _hologram.SetFloat("_Displacement", 
+            _curve.Evaluate(Time.timeSinceLevelLoad));
+    }
+}
